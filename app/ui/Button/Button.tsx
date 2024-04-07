f27cc8085420c9436c555d6c071bb11a13c0b5f4
Rogov Pavel <Benzer4254@gmail.com>
2024-04-07 17:20:03 +0300

    component button

--- app/ui/Button/Button.tsx
+++ app/ui/Button/Button.tsx
@@ -1,21 +1,29 @@
 "use client";
 
+import clsx from "clsx";
 import styles from "./style.module.scss";
 
 interface ButtonProps {
   onClick: () => void;
   children: React.ReactNode;
+  className?: string;
 }
 
-export const Button: React.FC<ButtonProps> = ({ onClick, children }) => {
+export const Button: React.FC<ButtonProps> = ({
+  onClick,
+  children,
+  className,
+}) => {
   return (
     <button
-      className={styles.button}
+      className={clsx(styles.button, className)}
       onClick={() => {
         onClick();
       }}
     >
-      {children}
+      <span className={styles.buttonText}>{children}</span>
+      <span className={styles.buttonAngleLeft} />
+      <span className={styles.buttonAngleRight} />
     </button>
   );
 };
